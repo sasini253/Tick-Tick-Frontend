38864f20faefa83f5e8f9d45adc058f510f87313
Sasini Maheesha <sashkavindi9819@gmail.com>
2021-11-23 10:59:00 +0530

    app.js

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React,{useState} from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from './Component/Header'
+import TodoItem from './Component/TodoItem';
+import AddToDo from './Component/AddToDo';
+
 
 export default function App() {
+
+  const[todos,setTodos] = useState([
+      {text:'Buy coffee',key:'1'},
+      {text:'Create an app',key:'2'},
+      {text:'Play on the switch',key:'3'}
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+
+      return prevTodos.filter(todo => todo.key!= key);
+    });
+  }
+  const submitHandler = (text) =>{
+
+    setTodos((prevTodos) => {
+      return[
+        {text:text,key:Math.random().toString()},
+        ...prevTodos
+      ]
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View styles={styles.content}>
+         <AddToDo submitHandler={submitHandler}/>
+        <View styles={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
